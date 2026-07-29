e7b9e35629af6f2ea572079b6a6211586166a041
rajesh <rajesh@gmail.com>
2026-07-29 21:34:25 +0530

    Fix cors typo

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="EMS SaaS API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_orig=["*"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
